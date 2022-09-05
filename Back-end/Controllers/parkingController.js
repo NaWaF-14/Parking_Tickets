ee83ff5e90305a100407d946bc435afc01022188
NaWaF-14 <Nawaf_M_98@hotmail.com>
2022-09-05 13:59:29 +0300

    this my last edit today

--- Back-end/Controllers/parkingController.js
+++ Back-end/Controllers/parkingController.js
@@ -1,5 +1,17 @@
 const Parking = require("../Models/parking");
 
+const addParking = async (req, res) => {
+  try {
+    await Parking.create({
+      city: req.body.city,
+      parkingLocation: req.body.parkingLocation,
+      parkingNumber: req.body.parkingNumber,
+    });
+    res.send("Parking is Added!");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 const allParking = async (req, res) => {
   try {
     const result = await Parking.find({});
@@ -13,16 +25,6 @@ const specificParking = async (req, res) => {
   try {
     const id = req.params.id;
     const result = await Parking.findById(id);
-    res.send(result);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-
-const updateParking = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const result = await Parking.findByIdAndUpdate(id);
     res.send(result);
   } catch (error) {
     console.log(error.message);
--- Back-end/Controllers/ticketController.js
+++ Back-end/Controllers/ticketController.js
@@ -1,9 +1,28 @@
 const Ticket = require("../Models/ticket");
 const User = require("../Models/user");
 
-const allUserTickets = async (req, res) => {
+// when the user purchase a ticket
+const purchaseTicket = async (req, res) => {
   try {
-    const id = req.params.id;
+    const userID = req.params.userID;
+    const parkingID = req.params.parkingID;
+    await Ticket.create({
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      price: req.body.price,
+      user: userID,
+      parkingLocation: parkingID,
+    });
+    res.send("User has purchased a ticket!!");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// display all the user tickets
+const userTickets = async (req, res) => {
+  try {
+    const id = req.params.id; // ID of the user
     const user = await User.findById(id);
     const result = await Ticket.find({ user: user._id });
     res.send(result);
@@ -11,3 +30,16 @@ const allUserTickets = async (req, res) => {
     console.log(error.message);
   }
 };
+
+// here when the user want to change the schedule
+const updateTicket = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await Ticket.findByIdAndUpdate(id, req.body);
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = { purchaseTicket, userTickets, updateTicket };
